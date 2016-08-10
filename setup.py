ff4cbc13703ecb3b2b147b6b5418365ee2d26a55
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-08-10 06:20:14 -0700

    Added __init__.py to package to resolve path issues

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'randomwordgenerator',
   packages = ['randomwordgenerator'],
-  version = '0.1',
+  version = '0.2',
   description = 'Python 3 Random Word Generator Library',
   author = 'Sairam Krishnan',
   author_email = 'sairambkrishnan@gmail.com',
   url = 'https://github.com/SaiWebApps/RandomWordGenerator',
-  download_url = 'https://github.com/SaiWebApps/RandomWordGenerator/tarball/0.1',
+  download_url = 'https://github.com/SaiWebApps/RandomWordGenerator/tarball/0.2',
   keywords = ['Python', 'random', 'word'],
   classifiers = [],
 )
