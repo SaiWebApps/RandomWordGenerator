f1abc1d2e3b8fefe811171a384ce7d40397092c2
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-09-02 19:11:16 -0700

    v1.0.0 of RandomWordGenerator - published to PyPI
    Previous commits (v0.1, v0.2) removed from PyPI.
    - v0.1 broken
    - v0.2 fixed v0.1, but the PyPI page did not display the README since it
      was in Github Markdown format, not RST format.
    This commit fixes the issues with v0.2 (incorporates lessons learned from
    publishing synonymscrawler today).

--- setup.py
+++ setup.py
@@ -1,14 +1,70 @@
 from distutils.core import setup
+from os import path
 
+import pypandoc
+
+# Constants
+VERSION = '1.0.0'
+THIS_DIR_ABS_PATH = path.abspath(path.dirname(__file__))
+README_ABS_PATH = path.join(THIS_DIR_ABS_PATH, 'README.md')
+
+# Tools to convert README from Github Markdown (MD) to RST format.
+def try_download_pandoc():
+  '''
+    @description
+    Try downloading pandoc if it's not already on the system.
+
+    @return True if we successfully downloaded pandoc, False otherwise
+  '''
+  try:
+    pypandoc.pandoc_download.download_pandoc()
+    return True
+  except:
+    return False
+
+def try_convert_to_rst(path):
+  '''
+    @description
+    Convert the file at the given absolute path to RST format.
+
+    @param path
+    Absolute path of the file that we want to convert to RST format.
+
+    @return the contents of the specified file in RST format. 
+  '''
+  file_contents = ''
+  try:
+    file_contents = pypandoc.convert(path, 'rst')
+  except:
+    file_contents = pypandoc.convert(path, 'rst') if try_download_pandoc() else ''
+  finally:
+    return file_contents
+
+# PyPI Package Configuration
 setup(
+  # Basic Package Details
   name = 'randomwordgenerator',
   packages = ['randomwordgenerator'],
-  version = '0.2',
+  version = VERSION,
+
+  # Package Overview and Extended Description (README)
   description = 'Python 3 Random Word Generator Library',
+  long_description = try_convert_to_rst(README_ABS_PATH),
+
+  # Dependencies
+  install_requires = [
+    'requests'
+  ],
+
+  # Author Details
   author = 'Sairam Krishnan',
   author_email = 'sairambkrishnan@gmail.com',
+  
+  # Github Repo Details
   url = 'https://github.com/SaiWebApps/RandomWordGenerator',
-  download_url = 'https://github.com/SaiWebApps/RandomWordGenerator/tarball/0.2',
-  keywords = ['Python', 'random', 'word'],
-  classifiers = [],
+  download_url = 'https://github.com/SaiWebApps/RandomWordGenerator/tarball/' + VERSION,
+  
+  # Misc Details
+  license = 'MIT',
+  keywords = ['Python', 'random-word']
 )
